d9e0edf242dede6e2cfe9cea038336515ca11f9d
Borhan Uddin <borhaninfos@gmail.com>
2022-01-28 00:28:13 +0600

    Update BookDetails.js

--- src/components/represent/BookDetails.js
+++ src/components/represent/BookDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function BookDetails(props) {
-  console.log(props.book);
   if (props.book === null) return <div></div>;
   return (
     <div>
-      <h1>{props.book.bookName}</h1>
+      <h1>
+        ( {props.book.id} ) - {props.book.bookName}
+      </h1>
       <p>{props.book.description}</p>
     </div>
   );
